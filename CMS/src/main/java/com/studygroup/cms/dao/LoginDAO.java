17acfb8f12ff36831f7e2fab68bc655374bade77
Deepan <deepanrajkumar@gmail.com>
2013-11-22 11:45:47 -0600

    Hibernate code skeletion added.

--- CMS/src/main/java/com/studygroup/cms/dao/LoginDAO.java
+++ CMS/src/main/java/com/studygroup/cms/dao/LoginDAO.java
@@ -3,24 +3,68 @@
  */
 package com.studygroup.cms.dao;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.studygroup.cms.domain.UserDetails;
 import com.studygroup.cms.interfaces.ILoginDAO;
 
 @Repository("loginDAO")
 public class LoginDAO implements ILoginDAO {
 	
-//	private SessionFactory sessionFactory;
-//    
-//    @Autowired
-//    public void setSessionFactory(SessionFactory sessionFactory) {
-//        this.sessionFactory = sessionFactory;
-//    }
+	@Autowired
+    @Qualifier("sessionFactory")
+	private SessionFactory sessionFactory;
     
+	
+         
 	public void validateLogin(String userName, String password) {
 		System.out.println("UserName: from DAO : " + userName);
 		
 		System.out.println("Password: from DAO : " + password);
+		
+		List<UserDetails> users = listUser();
+		if(users != null){
+			System.out.println("Number of users from DB : " + users.size());
+		} else {
+			System.out.println("No user from DB");
+		}
 	}
+	
+	
+	@Transactional
+	public void addUser(UserDetails user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
+	
+	@Transactional
+	public List<UserDetails> listUser() {
+		 //sessionFactory.getCurrentSession().createQuery("from UserDetails").list()
+		System.out.println("Inside List User....");
+		//Session s = sessionFactory.openSession();
+		if(sessionFactory != null){
+			System.out.println("Able to get session factory instance ....");
+			
+		} else {
+			System.out.println("Can't get session from factory instance ....");
+		}
+        return null;
+        
+    }
+	
+	@Transactional
+	public void removeUser(Integer id) {
+        UserDetails user = (UserDetails) sessionFactory.getCurrentSession().load(
+                UserDetails.class, id);
+        if (null != user) {
+            sessionFactory.getCurrentSession().delete(user);
+        }
+ 
+    }
 }
--- CMS/src/main/java/com/studygroup/cms/interfaces/ILoginDAO.java
+++ CMS/src/main/java/com/studygroup/cms/interfaces/ILoginDAO.java
@@ -3,7 +3,14 @@
  */
 package com.studygroup.cms.interfaces;
 
+import java.util.List;
+
+import com.studygroup.cms.domain.UserDetails;
+
 
 public interface ILoginDAO {
 	public void validateLogin(String userName, String password);
+	public void addUser(UserDetails user);
+	public List<UserDetails> listUser();
+	public void removeUser(Integer id);
 }
