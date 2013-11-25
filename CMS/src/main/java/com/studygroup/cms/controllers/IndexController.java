99212d7034d11df558539bab93404bee3a8edb0b
Deepan <deepanrajkumar@gmail.com>
2013-11-25 16:08:30 -0600

    Login page initial validation done.

--- CMS/src/main/java/com/studygroup/cms/controllers/IndexController.java
+++ CMS/src/main/java/com/studygroup/cms/controllers/IndexController.java
@@ -4,25 +4,49 @@
 package com.studygroup.cms.controllers;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.studygroup.cms.interfaces.ILoginService;
+import com.studygroup.cms.views.UserLoginViewBean;
 
 @Controller
 public class IndexController {
 	
+	
 	@Resource(name="loginService")
-	private ILoginService loginService;
+	private ILoginService loginService;	
 	
 	@RequestMapping(value="/login", method=RequestMethod.POST)
-    public ModelAndView loginToCMS(@RequestParam(value="usesrname", required=true) String userName, @RequestParam(value="password", required=true) String password) {
-		loginService.validateLogin(userName, password);
-        return new ModelAndView("home");
+    public ModelAndView loginToCMS(@Valid @ModelAttribute("subscriber") UserLoginViewBean user, BindingResult result, Model model) {
+		
+		
+		if(result.hasErrors()){ 
+			System.out.println("------- Has errors .... " + result.getErrorCount());
+			System.out.println("------- Has errors .... " + result.getAllErrors().get(0));
+			//model.addAttribute("subscriber", subscriber);
+			
+			return new ModelAndView("index");
+		} else {
+			
+			loginService.validateLogin(user.getUsername(), user.getPassword());
+			System.out.println("------- No errors ....");
+		}
+       return new ModelAndView("home");
+    }
+	
+	@RequestMapping(value="/index", method=RequestMethod.GET)
+    public String indexCMS(Model m) {
+    	m.addAttribute("subscriber", new UserLoginViewBean());
+        return "index";
     }
 	
 	@RequestMapping("/register")
--- CMS/src/main/webapp/WEB-INF/pages/index.jsp
+++ CMS/src/main/webapp/WEB-INF/pages/index.jsp
@@ -1,27 +1,43 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<style type="text/css">
+.error {
+	color: #ff0000;
+}
+ 
+.errorblock {
+	color: #000;
+	background-color: #ffEEEE;
+	border: 3px solid #ff0000;
+	padding: 8px;
+	margin: 16px;
+}
+</style>
 <title>Cab Management System</title>
 </head>
 <body style="text-align: center; border-width: 1px;">
 <h1>Cab Management System</h1>
 <br/><br/><br/><br/><br/>
-	<table>
+	<table >
 		<tr>
 			<td>
-				<form method="post" action="login.htm">
+				<form:form action="login.htm" modelAttribute="subscriber" method="POST">				
 				<table border="1px">
-					<tr>
-						<td>UserName : </td>
-						<td><input type="text" id="usesrname" name="usesrname" /></td>
+					<tr> 
+						<td><label for="usernameInput">User Name : </label></td>
+						<td><form:input path="username" id="usernameInput" /></td>
+						<td><form:errors path="username" cssClass="error" /></td>
 					</tr>
 					
 					<tr>
-						<td>Password : </td>
-						<td><input type="password" id="password" name="password" /></td>
+						<td><label for="passwordInput">Password : </label></td>
+						<td><form:input path="password" id="passwordInput" /></td>
+						<td><form:errors path="password" cssClass="error"  /></td>
 					</tr>
 					<tr>
 						<td>
@@ -29,7 +45,7 @@
 						</td>
 					</tr>
 				</table>
-				</form>
+				</form:form>
 			</td>
 			<td>
 				<form method="post" action="register.htm">
